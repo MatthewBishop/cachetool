4868e56df51a97fb24c1c2e02d52098c521b070f
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2022-10-08 12:24:09 -0400

    File sync

---
+++ src/com/jagex/cache/configs/Widget2.java
@@ -0,0 +1,47 @@
+package com.jagex.cache.graphics;
+
+public class Widget2  {
+
+	public int id;
+	public int parent;
+	public int group;
+	public int optionType;
+	public int contentType;
+	public int width;
+	public int height;
+	public byte alpha;
+	public int hoverId;
+	public int[] scriptOperators;
+	public int[] scriptDefaults;
+	public int[][] scripts;
+	public int scrollLimit;
+	public boolean hidden;
+	public int[] children;
+	public int[] childX;
+	public int[] childY;
+	public int[] inventoryIds;
+	public int[] inventoryAmounts;
+	public boolean swappableItems;
+	public boolean hasActions;
+	public boolean usableItems;
+	public boolean replaceItems;
+	public int spritePaddingX;
+	public int spritePaddingY;
+	public int[] spriteX;
+	public int[] spriteY;
+	public Sprite[] sprites;
+	public String[] actions;
+	public boolean filled;
+	public boolean centeredText;
+	public Font font;
+	public boolean shadowedText;
+	public String defaultText;
+	public String secondaryText;
+	public int defaultColour;
+	public int secondaryColour;
+	public int defaultHoverColour;
+	public int secondaryHoverColour;
+	public Sprite defaultSprite;
+	public Sprite secondarySprite;
+
+}
