8b7a2ac73c4389a21c75a205e6195f0af20048e5
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-04-20 22:56:46 -0400

    setup: gradle build+kt

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "dev.openrune"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/org.itadaki/bzip2
+    implementation("org.itadaki:bzip2:0.9.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
+}
